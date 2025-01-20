db1d60c78ffde1b145645e4f8db1db0a80f4f4b5
rouvb <rouvbkim@gmail.com>
2025-01-20 09:25:36 +0900

    Refactor: 임시방편으로 플레이어가 서버에 처음 접속 시에만 체력이 회복 되는 방식으로 수정

--- src/main/java/net/arkamc/randomHP/listener/PlayerJoinListener.java
+++ src/main/java/net/arkamc/randomHP/listener/PlayerJoinListener.java
@@ -6,15 +6,28 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class PlayerJoinListener implements Listener {
+
+    HashMap<UUID, Integer> map = new HashMap<>();
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        RandomHPTask task = new RandomHPTask();
         Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
 
-        if (task.randomHPTask == null) {
-            player.setMaxHealth(20);
+        if (map.containsKey(uuid)) {
+            map.put(uuid, map.get(uuid) + 1);
+        } else {
+            map.put(uuid, 1);
+        }
+
+        int count = map.get(uuid);
+
+        if (count < 2) {
+            player.setMaxHealth(20.0);
             player.setHealth(20.0);
         }
     }
